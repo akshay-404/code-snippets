e80f4ad1200eb3f201f6f3cbea491fc426cea935
Akshay Anil <akshay.a.cd.phy22@itbhu.ac.in>
2024-06-12 10:58:33 +0530

    added to db

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ class User:
     domain = 'itbhu.ac.in'
     branch = 'phy'
 
-    def __init__(self, name1, name2, contact) -> None:
-        self.firstname = name1.capitalize()
-        self.lastname = name2.capitalize()
-        self.name = f'{self.firstname} {self.lastname}'
+    def __init__(self, name, gender, contact) -> None:
+        self.name = name.title()
+        self.gender = gender
         self.contact = contact
+
+    def nameSplit(self):
+        x = self.name.split()
+        firstname = ''.join(x[:-1])
+        lastname = x[-1]
+        return firstname, lastname
 
     def __str__(self) -> str:
         return f'<{self.firstname}.{self.lastname}>'
 
     def __repr__(self) -> str:
-        return f'User(\'{self.firstname}\', \'{self.lastname}\', {self.contact})'
+        return f'User(\'{self.firstname}\', \'{self.lastname}\')'
     
 class Student(User):
-    pass
+    year = 2022
 
-
-x = User('Akshay', 'Anil', 8848940385)
-y = x
-
-
-print(y)
+    def __init__(self, name, gender, contact, roll) -> None:
+        super().__init__(name, gender, contact)
+        name1, name2 = self.nameSplit()
+        self.email = f'{name1.lower().replace(' ', '')}.{name2.lower()}.{User.branch}{Student.year%100}@{User.domain}'
+        self.roll = roll
